497bae21bf70d5f4dd7189f1cd31493b7f9ac0b2
Simon Moore <s.moore@kainos.com>
2023-03-21 14:06:38 +0000

    Enhance Redis connection to improve error handling

--- config/environment.js
+++ config/environment.js
@@ -1,44 +1,90 @@
-// /**
-//  * Created by preciousr on 07/01/2016.
-//  */
+require('dotenv').config();
+const cookiedomain = JSON.parse(process.env.COOKIEDOMAIN);
+const sequelizeusers = JSON.parse(process.env.SEQUELIZEUSERS);
+const servicesequelize = JSON.parse(process.env.SERVICESEQUELIZE);
+const applicationserviceurl = JSON.parse(process.env.APPLICATIONSERVICEURL);
+const notificationserviceurl = JSON.parse(process.env.NOTIFICATIONSERVICEURL);
+const passwordsettings = JSON.parse(process.env.PASSWORDSETTINGS);
+const postcodelookupoptions = JSON.parse(process.env.POSTCODELOOKUPOPTIONS);
+const live_variables = JSON.parse(process.env.LIVEVARIABLES);
+const userAccountSettings = JSON.parse(process.env.USERACCOUNTSETTINGS);
+const pgPassword = process.env.PGPASSWORD;
+const Sequelize = require('sequelize');
+const accountManagementApiUrl = process.env.ACCOUNTMANAGEMENTAPIURL;
+const certPath = process.env.NODE_ENV !== 'development' ? process.env.CASEBOOKCERTIFICATE : process.env.CASEBOOKCERTIFICATE.replace(/\\n/gm, '\n');
+const keyPath = process.env.NODE_ENV !== 'development' ? process.env.CASEBOOKKEY : process.env.CASEBOOKKEY.replace(/\\n/gm, '\n');
+const hmacKey = process.env.HMACKEY;
 
-// module.exports = {
+const sequelizeUsers = new Sequelize(
+    sequelizeusers.sequelizeusers.dbName,
+    sequelizeusers.sequelizeusers.dbUser,
+    sequelizeusers.sequelizeusers.dbPass,
+    {
+        host: sequelizeusers.userconnection.host,
+        port: sequelizeusers.userconnection.port,
+        dialect: 'postgres',
+        logging: false,
+        dialectOptions: {
+            'connectTimeout': 15000 // 15 seconds timeout
+        },
+        retry: {
+            base: 1000,
+            multiplier: 2,
+            max: 5000,
+        }
+    }
+)
 
-require('dotenv').config()
-var cookiedomain = JSON.parse(process.env.COOKIEDOMAIN);
-var sequelizeusers = JSON.parse(process.env.SEQUELIZEUSERS);
-var servicesequelize = JSON.parse(process.env.SERVICESEQUELIZE);
-var applicationserviceurl = JSON.parse(process.env.APPLICATIONSERVICEURL);
-var notificationserviceurl = JSON.parse(process.env.NOTIFICATIONSERVICEURL);
-var passwordsettings = JSON.parse(process.env.PASSWORDSETTINGS);
-var postcodelookupoptions = JSON.parse(process.env.POSTCODELOOKUPOPTIONS);
-var live_variables = JSON.parse(process.env.LIVEVARIABLES);
-var userAccountSettings = JSON.parse(process.env.USERACCOUNTSETTINGS);
-var pgPassword = process.env.PGPASSWORD;
-var Sequelize = require('sequelize');
-var accountManagementApiUrl = process.env.ACCOUNTMANAGEMENTAPIURL;
-var certPath = process.env.NODE_ENV !== 'development' ? process.env.CASEBOOKCERTIFICATE : process.env.CASEBOOKCERTIFICATE.replace(/\\n/gm, '\n');
-var keyPath = process.env.NODE_ENV !== 'development' ? process.env.CASEBOOKKEY : process.env.CASEBOOKKEY.replace(/\\n/gm, '\n');
-var hmacKey = process.env.HMACKEY;
+const serviceSequelize = new Sequelize(
+    servicesequelize.servicesequelize.dbName,
+    servicesequelize.servicesequelize.dbUser,
+    servicesequelize.servicesequelize.dbPass,
+    {
+        host: servicesequelize.serviceconnection.host,
+        port: servicesequelize.serviceconnection.port,
+        dialect: 'postgres',
+        logging: false,
+        dialectOptions: {
+            'connectTimeout': 15000 // 15 seconds timeout
+        },
+        retry: {
+            base: 1000,
+            multiplier: 2,
+            max: 5000,
+        }
+    }
+)
 
-var config = {
+//=========================
+// SEQUELIZE CONNECTION
+//=========================
+sequelizeUsers.authenticate().then(() => {
+    console.log(`Connection has been established to ${sequelizeusers.sequelizeusers.dbName} successfully.`);
+}).catch((error) => {
+    console.error(`Unable to connect to the ${sequelizeusers.sequelizeusers.dbName} database: ${error}`);
+});
+
+serviceSequelize.authenticate().then(() => {
+    console.log(`Connection has been established to ${servicesequelize.servicesequelize.dbName} successfully.`);
+}).catch((error) => {
+    console.error(`Unable to connect to the ${servicesequelize.servicesequelize.dbName} database: ${error}`);
+});
+
+const config = {
     "cookieDomain": cookiedomain,
-    "sequelizeUsers":new Sequelize(sequelizeusers.sequelizeusers.dbName, sequelizeusers.sequelizeusers.dbUser, sequelizeusers.sequelizeusers.dbPass,
-        {'host': sequelizeusers.userconnection.host,'port': sequelizeusers.userconnection.port, 'dialect': 'postgres', 'logging': false}),
-    "serviceSequelize":new Sequelize(servicesequelize.servicesequelize.dbName, servicesequelize.servicesequelize.dbUser, servicesequelize.servicesequelize.dbPass,
-        {'host': servicesequelize.serviceconnection.host,'port':servicesequelize.serviceconnection.port, 'dialect': 'postgres', 'logging': false}),
-    "applicationServiceURL":applicationserviceurl.applicationserviceurl,
-    "notificationServiceURL":notificationserviceurl.notificationserviceurl,
-    "password_settings":passwordsettings,
-    "postcodeLookUpApiOptions":postcodelookupoptions,
+    sequelizeUsers,
+    serviceSequelize,
+    "applicationServiceURL": applicationserviceurl.applicationserviceurl,
+    "notificationServiceURL": notificationserviceurl.notificationserviceurl,
+    "password_settings": passwordsettings,
+    "postcodeLookUpApiOptions": postcodelookupoptions,
     "pgpassword": pgPassword,
     "userAccountSettings": userAccountSettings,
-    "live_variables":live_variables,
-    "accountManagementApiUrl":accountManagementApiUrl,
-    "certPath":certPath,
-    "keyPath":keyPath,
-    "hmacKey":hmacKey
+    "live_variables": live_variables,
+    "accountManagementApiUrl": accountManagementApiUrl,
+    "certPath": certPath,
+    "keyPath": keyPath,
+    "hmacKey": hmacKey
 };
 
 module.exports = config;
-
