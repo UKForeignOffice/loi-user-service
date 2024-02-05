e771525f76021900f28a267bff9e587b258a83ee
Simon Moore <s.moore@kainos.com>
2024-02-05 10:36:01 +0000

    Remove log lines

--- app/services/HelperService.js
+++ app/services/HelperService.js
@@ -12,8 +12,6 @@ const HelperService = {
 
         if (cachedToken) {
             console.log('Returning access token from cache');
-            // TODO: REMOVE THIS AFTER UAT
-            console.log(cachedToken);
             return cachedToken;
         }
 
@@ -35,8 +33,6 @@ const HelperService = {
             const { access_token } = response.data;
             cache.set(cacheKey, access_token);
             console.log('Returning access token from EDMS');
-            // TODO: REMOVE THIS AFTER UAT
-            console.log(access_token);
             return access_token;
         } catch (error) {
             console.error('Error fetching access token from EDMS:', error);
