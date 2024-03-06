7fc454d16851f5a2e1634b8fa9727c9b2f382544
Simon Moore <s.moore@kainos.com>
2024-03-06 15:41:18 +0000

    Update tests

--- tests/specs/user.spec.js
+++ tests/specs/user.spec.js
@@ -1,29 +1,37 @@
-var expect  = require("chai").expect;
-var request = require("request");
+let expect;
+let server;
 
-before("Run Server", function (done) {
-    server = require("../../server").getApp;
-    done();
+before("Run Server", async function () {
+    const chai = await import("chai");
+    expect = chai.expect;
+    // Adjust the relative path as necessary and include the file extension
+    server = (await import("../../server.js")).getApp;
 });
+
 
 describe("Healthcheck is working", function() {
 
+    const url = "http://localhost:3001/api/user/healthcheck";
+
     describe("GET /healthcheck", function() {
 
-        var url = "http://localhost:3001/api/user/healthcheck";
-
         it("returns status 200", function(done) {
-            request(url, function(error, response, body) {
-                expect(response.statusCode).to.equal(200);
-                done();
-            });
+            fetch(url)
+                .then(response => {
+                    expect(response.status).to.equal(200);
+                    done();
+                })
+                .catch(err => done(err));
         });
 
         it("JSON body is correct", function(done) {
-            request(url, function(error, response, body) {
-                expect(body).to.contain('"message":"User Service is running"')
-                done();
-            });
+            fetch(url)
+                .then(response => response.json()) // Convert to JSON
+                .then(data => {
+                    expect(data.message).to.equal("User Service is running");
+                    done();
+                })
+                .catch(err => done(err));
         });
 
     });
